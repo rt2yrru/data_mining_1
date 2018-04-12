22711eba2636d3a7176a0067f827113ce9495f7d
siddht4 <dhar.75.ap@gmail.com>
2018-04-12 12:10:34 +0530

    Create dredger_layout.py

---
+++ dredger_layout.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import csv,json 
+json_file=input(' Enter the json file ')
+with open("json_file) as f1:
+    z = json.load(f1)    # json file is now read , z has json data and is pretty much a dictionary
+    
